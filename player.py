eec7cd4eb05a6742a27f4521a5f3937af929394c
OzYossarian <alex.townsend-teague@outlook.com>
2020-04-21 21:38:18 +0100

    Neaten up getPossibleActions

--- player.py
+++ player.py
@@ -51,65 +51,74 @@ class Player:
 
     def getPossibleActions(self, state):
         # Must only use the state to determine actions, nothing else.
-        actions = []
         openAttacks = getCards(state, self.openAttacks)
         closedAttacks = getCards(state, self.closedAttacks)
         defences = getCards(state, self.defences)
         cards = getCards(state, self.cards)
 
         if self.isDefender(state):
-            # Can't defend until we've been attacked.
             if length(openAttacks) == 0:
+                # Can't defend until we've been attacked.
                 return [partial(self.game.waitForUpdates, self.name, state)]
-
+            actions = []
             if length(closedAttacks) + length(defences) == 0:
-                # Can possibly bounce.
-                attackValue = openAttacks[1][0]
-                bounceCards = numpy.array(cards).T[numpy.where(cards[1] == attackValue)]
-                bounces = allSublists(bounceCards)
-                for bounce in bounces:
-                    actions.append(partial(self.game.bounce, self.name, state, tuple(bounce.T)))
-
-            # Can always concede.
-            # If there are more open attacks than we have cards, then only pick up as many as we have cards.
-            surplusAttacks = length(openAttacks) - length(cards)
-            if surplusAttacks > 0:
-                for attacks in combinations(numpy.array(openAttacks).T, surplusAttacks):
-                    actions.append(partial(self.game.concede, self.name, state, tuple(numpy.array(attacks).T)))
-            else:
-                actions.append(partial(self.game.concede, self.name, state, openAttacks))
-
-            # Can possibly defend
-            # ToDo: optimise all this switching between numpy arrays and tuples
-            for attack in numpy.array(openAttacks).T:
-                for card in numpy.array(cards).T:
-                    if self.canDefend(attack, card, state):
-                        actions.append(partial(self.game.defend, self.name, state, tuple(card.T), tuple(attack.T)))
+                actions.extend(self.bounceActions(cards, openAttacks, state))
+            actions.extend(self.concedeActions(cards, openAttacks, state))
+            actions.extend(self.defendActions(cards, openAttacks, state))
+            return actions
 
         elif self.isAttacker(state) and length(openAttacks) + length(closedAttacks) == 0:
-            # Can attack with multiple cards of the same value
-            for value in range(13):
-                valueCards = numpy.array(cards).T[numpy.where(cards[1] == value)]
-                attacks = allSublists(valueCards)
-                for attack in attacks:
-                    actions.append(partial(self.game.attack, self.name, state, tuple(attack.T)))
+            return self.attackActions(cards, state)
 
         # Can only attack if defender has elected to defend and there are fewer than 'maxAttacks' attacks already.
         elif length(defences) > 0 and length(openAttacks) + length(closedAttacks) < self.game.maxAttacks:
-            # Can always decline to attack
-            actions.append(partial(self.game.declineToAttack, self.name, state))
-            # Can only attack with cards whose values are already on the table.
-            valuesAllowed = numpy.concatenate((closedAttacks[1], defences[1]), axis=None)
-            indices = numpy.where(numpy.isin(cards[1], valuesAllowed))[0]
-            for i in indices:
-                card = (cards[0][i], cards[1][i])
-                actions.append(partial(self.game.joinAttack, self.name, state, card))
+            actions = [partial(self.game.declineToAttack, self.name, state)]
+            actions.extend(self.joinAttackActions(cards, closedAttacks, defences, state))
+            return actions
 
         else:
-            # Have to wait for updates.
-            actions.append(partial(self.game.waitForUpdates, self.name, state))
+            return [partial(self.game.waitForUpdates, self.name, state)]
 
+    def concedeActions(self, cards, openAttacks, state):
+        # If there are more open attacks than we have cards, then only pick up as many as we have cards.
+        actions = []
+        surplusAttacks = length(openAttacks) - length(cards)
+        if surplusAttacks > 0:
+            for attacks in combinations(numpy.array(openAttacks).T, surplusAttacks):
+                actions.append(partial(self.game.concede, self.name, state, tuple(numpy.array(attacks).T)))
+        else:
+            actions.append(partial(self.game.concede, self.name, state, openAttacks))
         return actions
+
+    def joinAttackActions(self, cards, closedAttacks, defences, state):
+        # Can only attack with cards whose values are already on the table.
+        valuesAllowed = numpy.concatenate((closedAttacks[1], defences[1]), axis=None)
+        indices = numpy.where(numpy.isin(cards[1], valuesAllowed))[0]
+        for i in indices:
+            card = (cards[0][i], cards[1][i])
+            yield partial(self.game.joinAttack, self.name, state, card)
+
+    def attackActions(self, cards, state):
+        # Can attack with multiple cards of the same value
+        for value in range(13):
+            valueCards = numpy.array(cards).T[numpy.where(cards[1] == value)]
+            attacks = allSublists(valueCards)
+            for attack in attacks:
+                yield partial(self.game.attack, self.name, state, tuple(attack.T))
+
+    def defendActions(self, cards, openAttacks, state):
+        # ToDo: optimise all this switching between numpy arrays and tuples
+        for attack in numpy.array(openAttacks).T:
+            for card in numpy.array(cards).T:
+                if self.canDefend(attack, card, state):
+                    yield partial(self.game.defend, self.name, state, tuple(card.T), tuple(attack.T))
+
+    def bounceActions(self, cards, openAttacks, state):
+        attackValue = openAttacks[1][0]
+        bounceCards = numpy.array(cards).T[numpy.where(cards[1] == attackValue)]
+        bounces = allSublists(bounceCards)
+        for bounce in bounces:
+            yield partial(self.game.bounce, self.name, state, tuple(bounce.T))
 
     def isDefender(self, state):
         return state[self.defender][0][0] == 0
