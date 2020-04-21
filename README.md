b856b587254a0989d3abbee63aa2a81e55fd55d3
OzYossarian <alex.townsend-teague@outlook.com>
2020-04-21 02:26:00 +0100

    Switching to using numpy arrays

--- README.md
+++ README.md
@@ -11,8 +11,14 @@ a player will need to update their beliefs of other people's cards.
 
 - Experiment with scoring functions. One idea: once out of cards, player's score is number of cards held by all other
 players, with loser's score being negative the number of cards they had left. Another: score is 1 for anyone who
-didn't lose, with -1 for the loser.
+didn't lose, with -1 for the loser. The latter is more in keeping with Durak spirit - 'no winner, only a loser'.
 
 - Possible simplifications:
-  - Strict turn-taking - no race to see whose action is accepted
+  - Strict turn-taking - no race to see whose action is accepted. Might well be perfectly fine to train on a strict
+  turn-taking version then play on a real game anyway?
   - One vs one (or one vs random)
+
+Scribblings:
+
+- How to handle situation where agent tries to perform action but action is rejected? Is it enough to just return
+a reward of 0 (as usual)? Or do we need to say "the action was rejected, don't learn anything from this" somehow?
--- player.py
+++ player.py
@@ -3,13 +3,15 @@ import time
 from functools import partial
 from itertools import combinations
 
-from game import getCards
+import numpy
+
+from game import getCards, numberOfCards, length
 
 
 def allSublists(xs, maxSize=None):
     if maxSize is None:
         maxSize = len(xs)
-    return [list(ys) for i in range(1, maxSize + 1) for ys in combinations(xs, i)]
+    return [numpy.array(ys) for i in range(1, maxSize + 1) for ys in combinations(xs, i)]
 
 
 class Player:
@@ -19,6 +21,8 @@ class Player:
         self.name = name
         self.game = game
 
+        # ToDo: player should hold and update beliefs about other players' cards.
+        self.cards = 0
         self.trumps = 1
         self.openAttacks = 2
         self.closedAttacks = 3
@@ -40,10 +44,10 @@ class Player:
             self.game.done(self.name, None)
 
     def hasCards(self, state):
-        return sum([sum(suit) for suit in state[0]]) > 0
+        return numberOfCards(state, self.cards) > 0
 
     def hasLost(self, state):
-        return sum([sum(suit) for suit in state[0]]) + sum([sum(suit) for suit in state[self.burned]]) == 52
+        return numberOfCards(state, self.cards) + numberOfCards(state, self.burned) == 52
 
     def getPossibleActions(self, state):
         # Must only use the state to determine actions, nothing else.
@@ -51,50 +55,55 @@ class Player:
         openAttacks = getCards(state, self.openAttacks)
         closedAttacks = getCards(state, self.closedAttacks)
         defences = getCards(state, self.defences)
-        cards = getCards(state, 0)
+        cards = getCards(state, self.cards)
 
         if self.isDefender(state):
             # Can't defend until we've been attacked.
-            if len(openAttacks) == 0:
+            if length(openAttacks) == 0:
                 return [partial(self.game.waitForUpdates, self.name, state)]
 
-            if len(closedAttacks) + len(defences) == 0:
+            if length(closedAttacks) + length(defences) == 0:
                 # Can possibly bounce.
-                attackValue = openAttacks[0][0]
-                bounceCards = [(value, suit) for (value, suit) in cards if value == attackValue]
+                attackValue = openAttacks[1][0]
+                bounceCards = numpy.array(cards).T[numpy.where(cards[1] == attackValue)]
                 bounces = allSublists(bounceCards)
                 for bounce in bounces:
-                    actions.append(partial(self.game.bounce, self.name, state, bounce))
+                    actions.append(partial(self.game.bounce, self.name, state, tuple(bounce.T)))
 
             # Can always concede.
             # If there are more open attacks than we have cards, then only pick up as many as we have cards.
-            surplusAttacks = len(openAttacks) - len(cards)
+            surplusAttacks = length(openAttacks) - length(cards)
             if surplusAttacks > 0:
-                for attacks in combinations(openAttacks, surplusAttacks):
-                    actions.append(partial(self.game.concede, self.name, state, list(attacks)))
+                for attacks in combinations(numpy.array(openAttacks).T, surplusAttacks):
+                    actions.append(partial(self.game.concede, self.name, state, tuple(numpy.array(attacks).T)))
             else:
                 actions.append(partial(self.game.concede, self.name, state, openAttacks))
 
             # Can possibly defend
-            for attack in openAttacks:
-                for card in cards:
+            # ToDo: optimise all this switching between numpy arrays and tuples
+            for attack in numpy.array(openAttacks).T:
+                for card in numpy.array(cards).T:
                     if self.canDefend(attack, card, state):
-                        actions.append(partial(self.game.defend, self.name, state, card, attack))
+                        actions.append(partial(self.game.defend, self.name, state, tuple(card.T), tuple(attack.T)))
 
-        elif self.isAttacker(state) and len(openAttacks) + len(closedAttacks) == 0:
+        elif self.isAttacker(state) and length(openAttacks) + length(closedAttacks) == 0:
             # Can attack with multiple cards of the same value
-            for v in range(13):
-                vCards = [(value, suit) for (value, suit) in cards if value == v]
-                attacks = allSublists(vCards)
-                [actions.append(partial(self.game.attack, self.name, state, attack)) for attack in attacks]
+            for value in range(13):
+                valueCards = numpy.array(cards).T[numpy.where(cards[1] == value)]
+                attacks = allSublists(valueCards)
+                for attack in attacks:
+                    actions.append(partial(self.game.attack, self.name, state, tuple(attack.T)))
 
-        # Can only attack if defender has elected to defend and there are fewer than five attacks already.
-        elif len(defences) > 0 and len(openAttacks) + len(closedAttacks) < 5:
-            # Can only attack with cards whose values are already on the table.
+        # Can only attack if defender has elected to defend and there are fewer than 'maxAttacks' attacks already.
+        elif length(defences) > 0 and length(openAttacks) + length(closedAttacks) < self.game.maxAttacks:
+            # Can always decline to attack
             actions.append(partial(self.game.declineToAttack, self.name, state))
-            for card in cards:
-                if any(value == card[0] for (value, _) in openAttacks + closedAttacks + defences):
-                    actions.append(partial(self.game.joinAttack, self.name, state, card))
+            # Can only attack with cards whose values are already on the table.
+            valuesAllowed = numpy.concatenate((closedAttacks[1], defences[1]), axis=None)
+            indices = numpy.where(numpy.isin(cards[1], valuesAllowed))[0]
+            for i in indices:
+                card = (cards[0][i], cards[1][i])
+                actions.append(partial(self.game.joinAttack, self.name, state, card))
 
         else:
             # Have to wait for updates.
@@ -109,8 +118,8 @@ class Player:
         return state[self.attacker][0][0] == 0
 
     def canDefend(self, attack, card, state):
-        (attackValue, attackSuit) = attack
-        (value, suit) = card
+        (attackSuit, attackValue) = tuple(attack)
+        (suit, value) = tuple(card)
         if suit == attackSuit:
             return value > attackValue
         else:
